e3dec56e146fe0f95ca09fc315349120facb545f
Oli Carter <olicartr@gmail.com>
2021-02-04 21:22:33 +0200

    improve reliability of (in/de)crementing order items

---
+++ src/forms/CheckoutForm/components/index.js
@@ -0,0 +1,5 @@
+export * from './OrderDeliveryAddressDeliveryInstructionsInput';
+export * from './OrderDeliveryAddressNameInput';
+export * from './OrderDeliveryAddressPhoneInput';
+export * from './OrderDeliveryAddressStreetInput';
+export * from './OrderDeliveryAddressPostcodeInput';
